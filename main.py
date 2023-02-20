ac83099e95ebde365a1068613d979e9f3225a94b
Pablo Lacueva <71332626+SrKa0s@users.noreply.github.com>
2023-02-20 14:04:04 +0100

    token de log-in creado
    El sistema se conecta a la base de datos para identificar un usuario, y devuelve un token de sesión (como una cookie)

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import jwt
+
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware # https://fastapi.tiangolo.com/tutorial/cors/
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
@@ -10,6 +12,8 @@ from models.user import User
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = FastAPI()
+
+JWT_SECRET = "mysecret"
 
 origins = [
   "https://localhost:3000" # reacts'
@@ -28,16 +32,24 @@ session = get_session()
 
 # TODO : mirar como se hace el login y sesiones con Oauth2
 @app.post("/token")
-async def login(user_email : str, password : str):
-    user_dict = session.query(User).filter_by(email=user_email).first()
-    return user_dict
-    # if not user_dict:
-    #     raise HTTPException(status_code=400, detail="Incorrect username or password")
-    # user = UserInDB(**user_dict)
-    # hashed_password = generate_password_hash(form_data.password, "sha256")
-    # if not hashed_password == user.hashed_password:
-    #     raise HTTPException(status_code=400, detail="Incorrect username or password")
+def token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = session.query(User).filter_by(email=form_data.username).first()
+    if not user:
+        raise HTTPException(status_code=400, detail="Incorrect email")
+    if not check_password_hash(user.password, form_data.password):
+        raise HTTPException(status_code=400, detail="Incorrect password")
+    
+    user_dict = {"id": user.id, "username": user.username, "email": user.email, "password": user.password}
 
-    # return {"access_token": user.username, "token_type": "bearer"}
+    token = jwt.encode(user_dict, JWT_SECRET)
 
+    return {"access_token": token, "token_type": "bearer"}
 
+@app.post("/")
+def index(token: str = Depends(oauth2_scheme)):
+    if not token:
+        raise HTTPException(status_code=401, detail="Not authenticated")
+        #TODO: return something when not authenticated
+    else:
+        #returns the token
+        return {"token": token}
