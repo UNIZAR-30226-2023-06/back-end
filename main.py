f75805a50f87bd045680828d5c2584212f9fb847
Pablo Lacueva <71332626+SrKa0s@users.noreply.github.com>
2023-03-19 11:20:08 +0100

    gets para obtener user por ID o por EMAIL añadidos

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import re
 import jwt
 from db import get_engine_from_settings
+from models.user import User
 from utils import *
 
 from fastapi import FastAPI, Depends, HTTPException
@@ -35,6 +37,32 @@ def index(token: str = Depends(oauth2_scheme)):
         #TODO: return something when authenticated
         #returns the token
         return {"token": token}
+
+
+from routes.auth import session
+#gets a user from the database given the user id
+@app.get("/get-user-from-id/{user_id}")
+def get_user(user_id: int):
+    user = session.query(User).filter(User.id == user_id).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    else:
+      return user
+    
+#gets a user from the database given the user email
+@app.get("/get-user-from-email/{email}")
+def get_user_by_email(email: str):
+    #if the email is not a valid email, return an error
+    if not is_valid_email(email):
+        raise HTTPException(status_code=400, detail="Invalid email format")
+  
+    user = session.query(User).filter(User.email == email).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    else:
+      return user
+
+is_valid_email = lambda email: re.match(r"[^@]+@[^@]+\.[^@]+", email)
 
 app.include_router(auth_router)
 app.include_router(friends_router)
