9a31084e7b27ff1ba204cd3777c1d850a9e3a0f4
Pablo Lacueva <71332626+SrKa0s@users.noreply.github.com>
2023-03-22 18:09:02 +0100

    cambios del registro revertidos

--- routes/auth.py
+++ routes/auth.py
@@ -39,19 +39,19 @@ def login(form_data: OAuth2PasswordRequestForm = Depends()):
 
 from schemas.user import UserBase, UserCreate
 @router.post("/register", tags=["auth"])
-def register(username: str, email: str, password: SecretStr):
+def register(user: str = Depends(UserCreate)):
 # def register(token: str = Depends(oauth2_scheme), user: str, email: str, password: str):
-    if session.query(User).filter_by(email=email).first() is None:
+    if session.query(User).filter_by(email=user.email).first() is None:
         # generates a random 4 digit number to use as id. If that number is already in use, it will generate another one
         user_id = random.randint(1000, 9999)
         while session.query(User).filter_by(id=user_id).first() is not None:
             user_id = random.randint(1000, 9999)
 
-        hashed_password = generate_password_hash(password.get_secret_value(), method='sha256')
-        new_user = User(id=user_id, username=username, email=email, password=hashed_password)
+        hashed_password = generate_password_hash(user.password.get_secret_value(), method='sha256')
+        new_user = User(id=user_id, username=user.name, email=user.email, password=hashed_password)
         session.add(new_user)
         session.commit()
-        user_dict = {"id": new_user.id, "username": username, "email": email}
+        user_dict = {"id": new_user.id, "username": user.name, "email": user.email}
         token = jwt.encode(user_dict, JWT_SECRET)
         return {"access_token": token, "token_type": "bearer", "detail": "User created"}
     else:
